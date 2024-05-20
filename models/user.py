c3888ebc57b1ed6df25099f7868e4b8ef245b66c
talk2dennis <adigwedennis@yahoo.com>
2024-05-20 18:05:56 +0100

    updated User model

--- models/user.py
+++ models/user.py
@@ -9,6 +9,7 @@ from sqlalchemy import Column, String, Boolean, Text, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 from hashlib import md5
 from models.category import user_category, Category
+import bcrypt
 
 
 class User(BaseModel, Base):
@@ -16,10 +17,10 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        phone = Column(String(15), nullable=True)
+        _password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        phone = Column(String(15), nullable=False)
         profile_pic = Column(Text, nullable=True)
         is_farmer = Column(Boolean, default=False)
         farm_name = Column(String(100), nullable=True)
@@ -40,3 +41,13 @@ class User(BaseModel, Base):
         if name == "password":
             value = md5(value.encode()).hexdigest()
         super().__setattr__(name, value)
+
+    def set_password(self, password):
+        """ hashes the user password using bycript """
+        self._password = bcrypt.hashpw(password.encode('utf-8'),
+                                       bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        """ checkes the hashed password for authentication """
+        return bcrypt.checkpw(password.encode('utf-8'),
+                              self._password.encode('utf-8'))

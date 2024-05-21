3b72ffa2dd1d5569d824d1c9c4e6c8a54fe6722f
talk2dennis <adigwedennis@yahoo.com>
2024-05-21 11:02:53 +0100

    set up status and users api

---
+++ api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Index """
+from models.category import Category
+from models.user import User
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Category, User]
+    names = ["categories", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)

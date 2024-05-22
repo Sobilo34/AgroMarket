6ddaa714d513c9f1db66c09f063f62317b6201c1
talk2dennis <adigwedennis@yahoo.com>
2024-05-22 10:59:33 +0100

    set up flask to serve static content

---
+++ web_app/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.category import Category
+from models.user import User
+from os import environ, getenv
+from flask import Flask, render_template
+import uuid
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ the index page of AgroMarket """
+
+    return render_template('index.html', cache_id = str(uuid.uuid4()))
+
+
+if __name__ == "__main__":
+    """ start app """
+    port = getenv('AGRO_API_PORT')
+    host = getenv('AGRO_API_HOST')
+    app.run(host, port)

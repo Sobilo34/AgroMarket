67a72d13ab162c52d5f7d49daad6e69c467965f9
Sobilo34 <bilalsolih60@gmail.com>
2024-05-26 04:11:51 +0100

    merge the Front end and backend of product upload together

---
+++ web_app/static/scripts/product.js
@@ -0,0 +1,24 @@
+// JavaScript code for dynamically appending product information
+fetch('http://192.168.99.185:5001/api/v1/products')
+.then(response => response.json())
+.then(data => {
+    data.forEach(product => {
+        const productElement = document.createElement('div');
+        productElement.classList.add('a_product');
+
+        const imageElement = document.createElement('img');
+        imageElement.src = '../static/images/farm 4.jpg';
+        imageElement.alt = 'product_img';
+        productElement.appendChild(imageElement);
+
+        const headingElement = document.createElement('h5');
+        headingElement.innerHTML = `<span id="des_caption">${product.name}</span><br>
+                                    Description: ${product.description}<br>
+                                    Price: ${product.price}<br>
+                                    Quantity: ${product.quantity}`;
+        productElement.appendChild(headingElement);
+
+        document.querySelector('.product_content').appendChild(productElement);
+    });
+})
+.catch(error => console.error('Error fetching products:', error));

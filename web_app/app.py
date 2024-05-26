791d99793d97285a3734a8da2039cc3e385161b7
talk2dennis <adigwedennis@yahoo.com>
2024-05-26 16:07:26 +0100

    impleted the create account

--- web_app/app.py
+++ web_app/app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import requests
+import json
 from models import storage
 from models.category import Category
 from models.user import User
 from os import environ, getenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 import uuid
 
 
 app = Flask(__name__)
+app.secret_key = 'ab2351a4ee4743dcf4fc89ff8523e734bc55c40ad7ab3b01614a5fe553d5d367'
 
 
 @app.teardown_appcontext
@@ -23,10 +26,21 @@ def index():
 
     return render_template('index.html', cache_id=str(uuid.uuid4()))
 
-@app.route('/signup', strict_slashes=False)
+@app.route('/signup', methods=['GET', 'POST'], strict_slashes=False)
 def signup_page():
-    """ the page for signing/logging in"""
-
+    """ the page for creating a user account """
+    if request.method == 'POST':
+        url = getenv('AGRO_API_URL') + '/users'
+        data = request.form.to_dict()
+        data_json = json.dumps(data)
+        response = requests.post(url, data=data_json,
+                                 headers={'Content-Type': 'application/json'})
+        if response.status_code == 201:
+            flash('Account created successfully', 'success')
+            return redirect(url_for('login_page'))
+        else:
+            flash('Account creation failed, check the form', 'danger')
+            return redirect(url_for('signup_page', data=data))
     return render_template('signup.html', cache_id=str(uuid.uuid4()))
 
 @app.route('/login', strict_slashes=False)
